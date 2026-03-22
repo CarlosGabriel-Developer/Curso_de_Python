636b2b183c78bcd8001a473909660640ec303c39
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-22 15:57:18 -0300

    Progressao Aritmetica

--- Modulo 02/exer51-progressao-aritmetica.py
+++ Modulo 02/exer51-progressao-aritmetica.py
@@ -1,16 +1,13 @@
 #Exercício Python 051: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão
 
 
-an = int(input("Qual é o termo que voce quer descobrir : "))
 
-a1 = int(input("Qual é o primeiro termo : "))
+termo_A1 = int(input("Primeiro termo :"))
+razão = int(input("Razâo :"))
 
-n = int(input("Posição do termo : "))
+termo_A10 = termo_A1 + (10-1) * razão
 
-r = int(input("Razão :"))
-
-
-
-resultado = an=a1 + (n-1) * r
-
-print(resultado)
+for i in range(termo_A1,termo_A10+razão,razão):
+    print(f"{i}", end="==>")
+    
+print("ACABOU")
