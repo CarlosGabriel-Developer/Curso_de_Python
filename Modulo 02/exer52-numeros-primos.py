8c37854cd0c2293c7892b21fd9840622da7d9594
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-22 17:26:57 -0300

    Estrutura de controle

--- Modulo 02/exer52-numeros-primos.py
+++ Modulo 02/exer52-numeros-primos.py
@@ -1,10 +1,24 @@
 #Exercício Python 052: Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 
 contador = 0
+print()
 num = int(input("Digite um numero : "))
+print()
 
 for i in range(1,num+1):
-    
+
     if num % i == 0:
         print(f"\033[0;31m{i}", end=",")
+        contador += 1 
         
+    else :
+        print(f"\033[0;34m{i}", end=",")
+        
+
+print(f"\n\033[mO numero {num} foi divisivel {contador} vezes")
+
+if contador == 2 :
+    print("Esse numero é primo")
+        
+else:
+    print("Esse numero não é primo")
