cc301be139eee986c3de8bc77a6014a2c252b8dc
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-22 12:18:51 -0300

    Formula do termo

--- Modulo 02/exer51-progressao-aritmetica.py
+++ Modulo 02/exer51-progressao-aritmetica.py
@@ -1,1 +1,16 @@
 #Exercício Python 051: Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão
+
+
+an = int(input("Qual é o termo que voce quer descobrir : "))
+
+a1 = int(input("Qual é o primeiro termo : "))
+
+n = int(input("Posição do termo : "))
+
+r = int(input("Razão :"))
+
+
+
+resultado = an=a1 + (n-1) * r
+
+print(resultado)
