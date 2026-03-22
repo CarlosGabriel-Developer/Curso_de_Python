2008573f6f4f9b9dfbd7cffa00747bb0e4ca1738
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-22 11:19:34 -0300

    Estruturas de Controle

--- Modulo 02/exer 50 - Soma de seis elementos.py
+++ Modulo 02/exer 50 - Soma de seis elementos.py
@@ -1,12 +1,18 @@
+#Exercício Python 050: Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
 
-soma = 0 
+soma_numPares = 0
+contador = 0
 
-for i in range (0,6,1):
 
-    num = int(input("Digite um numero : "))
+for i in range(1,7):
+    num = int(input("Digite um numero :"))
     
-    if num % 2 == 0:
-        soma += num
+    if num % 2 == 0 : 
+        contador += 1 
+        soma_numPares += num
+            
+print(f"Você digitou {contador}\nA soma dos numeros pares foi de {soma_numPares}")
         
-print(soma)
         
+    
+    
