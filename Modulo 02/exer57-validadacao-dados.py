778b63c199769810a1840ae8ba520655d0fadf88
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-27 18:43:15 -0300

    add

--- Modulo 02/exer57-validadacao-dados.py
+++ Modulo 02/exer57-validadacao-dados.py
@@ -1,2 +1,11 @@
 ##Exercício Python 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto
 
+sexo = str(input("O sexo da pessoa é [M/F] : "))
+
+while sexo not in "MmFf" : 
+    
+    print("Tente novamente")
+    
+    sexo = str(input("O sexo da pessoa é [M/F] : "))
+    
+print("Bem vindo[a]")
