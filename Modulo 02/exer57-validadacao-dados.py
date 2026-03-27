5c7b6271b4127cfe52d359347f751c739ca1e7b7
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-03-27 18:54:40 -0300

    add

--- Modulo 02/exer57-validadacao-dados.py
+++ Modulo 02/exer57-validadacao-dados.py
@@ -7,5 +7,19 @@ while sexo not in "MmFf" :
     print("Tente novamente")
     
     sexo = str(input("O sexo da pessoa é [M/F] : "))
+
+
+
+if sexo in "Mm":
     
-print("Bem vindo[a]")
+    print("=="*10)    
+    print("BEM VINDO")
+    print("=="*10)
+
+    
+elif sexo in "Ff":
+    
+    print("=="*10)    
+    print("BEM VINDA")
+    print("=="*10)
+    
