01dbea2c87c74426252992be0cd296f6ba28904d
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-03 18:21:51 -0300

    fix fibonacci

--- Modulo 02/exer63-sequeci-fibonacci1.py
+++ Modulo 02/exer63-sequeci-fibonacci1.py
@@ -1,3 +1,15 @@
 ##Exercício Python 063: Escreva um programa que leia um número N inteiro qualquer e mostre na tela os N primeiros elementos de uma Sequência de Fibonacci. 
 
-##primeiro_numero = int(input("Digite um numero : "))
+
+# Leitura do número N
+n = int(input("Digite quantos elementos da sequência de Fibonacci deseja ver: "))
+
+# Inicialização da sequência
+a, b = 0, 1
+cont = 0
+
+# Loop para gerar os N primeiros elementos
+while cont < n:
+    print(a, end=' ')
+    a, b = b, a + b  # Atualiza os valores de a e b
+    cont += 1
