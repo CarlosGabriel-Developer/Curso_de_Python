09efa866022e509edd00b4459e9d2d3404f4a01e
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-04 16:04:34 -0300

    add

--- Modulo 02/exer65-maior-menores-valores.py
+++ Modulo 02/exer65-maior-menores-valores.py
@@ -1,34 +1,37 @@
 ##Exercício Python 065: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 
-num = int(input("Digite um numero : "))
-soma = media = maior = menor = 0 
+contador_numeros = soma_numeros = numero_maior = numero_menor = 0 
 
-resposta = input("Deseja continuar [S/N] : ")
 
-if resposta in "Ss" : 
+numero = int(input("Digite um numero : "))
 
-    while resposta in "Ss" :
+resposta = input("Deseja continuar [S/N] : ").upper().strip()
+
+contador_numeros += 1 
+soma_numeros += numero
+
+while resposta in "Ss" : 
+    
+    if numero > numero_maior : 
         
-        soma += num
-        media += 1 
+        numero_maior = numero
+    
+    elif numero  < numero_menor : 
         
-        for i in num () : 
-            
-            if i == 1 : 
-                
-                maior = menor =  num 
-                
-            else : 
-                
-                if num > maior :
-                    
-                    maior = num
-                    
-                if num < menor : 
-                    
-                    menor = num 
-                
+        numero_menor = numero
         
-else : 
-    print("Fim do programa")
+    
+    numero = int(input("Digite um numero : "))
+
+    contador_numeros += 1 
+    soma_numeros += numero
+
+    resposta = input("Deseja continuar [S/N] : ").upper().strip()
         
+        
+                
+print("Fim do programa")
+print(f"Você digitou {contador_numeros} numeros")
+print(f"A soma dos numeros é de {soma_numeros}")
+print(f"A media dos numeros é de {soma_numeros/contador_numeros}")
+    
