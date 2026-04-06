1602ae80f1503ac1acd318516c794d2700b4a802
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-06 19:20:13 -0300

    versao ensinada em aula

---
+++ Modulo 02/exer65-maior-menores-valores-2.py
@@ -0,0 +1,29 @@
+respota = "s"
+
+soma = quatidade = media = maior = menor = 0 
+
+while respota in "Ss" :
+    
+    num = int(input("Digite um numero : "))
+    soma += num
+    quatidade += 1 
+    
+    if quatidade == 1 :
+        
+        maior = menor = num
+        
+    else :
+        
+        if num > maior :
+            
+            maior = num
+            
+        if num < menor :
+            
+            menor = num 
+            
+    respota = str(input("Quer continuar ? [S/N]")).upper().strip()[0]
+    
+media = soma / quatidade
+
+print()
