7ab21edd39a1c8fd1e49587345c09e64581118fa
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-08 19:17:22 -0300

    revisão de codigos mais antigos, e melhorias

--- Modulo 01/exer00-cores-no-terminal.py
+++ Modulo 01/exer00-cores-no-terminal.py
@@ -2,14 +2,14 @@ texto = str("carlos gabriel sousa")
 
 print(texto)
 
-print(f'\033[30m{texto}') #fonte branco
+print(f'\033[37m{texto}') #fonte branco
 print(f'\033[31m{texto}') #fonte vermelha
 print(f'\033[32m{texto}') #fonte verde
 print(f'\033[33m{texto}') #fonte amarela
 print(f'\033[34m{texto}') #fonte azul
 print(f'\033[35m{texto}') #fonte roxo
 print(f'\033[36m{texto}') #fonte ciano
-print(f'\033[37m{texto}') #fonte cinza
+print(f'\033[30m{texto}') #fonte cinza
 
 
 print(f'\033[1;34m{texto}') #a fonte fica em negrito
--- Modulo 01/exer06-numeros-seus-mult.py
+++ Modulo 01/exer06-numeros-seus-mult.py
@@ -4,3 +4,7 @@ print(f"O seu numero é {numero}")
 print(f"O dobro do seu numero é {numero*2}")
 print(f"O tribloe do seu numero é {numero*3}")
 print(f"A raiz quadrada do seu numero é {numero**0.5:.2f}")
+
+print("===REVISÃO===")
+print(f"O seu numero é\033[1;32m {numero}\033[0m")
+print(f"O dobro do seu numero é\033[1;32m {numero*2}\033[0m")
