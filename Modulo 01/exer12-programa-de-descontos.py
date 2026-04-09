cca8bb95e6d8208d2f80263c67d8487d13057ffb
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-09 20:14:01 -0300

    revisão de codigos mais antigos, e melhorias

--- Modulo 01/exer12-programa-de-descontos.py
+++ Modulo 01/exer12-programa-de-descontos.py
@@ -3,3 +3,10 @@ produto = float(input("Digite o valor do produto : "))
 desc = (produto - (produto *0.05))
 
 print(f"Esse é o valor do produto {produto} e com um desconto de 5% {desc}")
+
+
+
+print("==REVISÃO==")
+
+print(f"Esse é o valor do produto \033[1;34m{produto}\033[0m e com um desconto de 5% \033[1;32m{desc}\033[0m")
+
