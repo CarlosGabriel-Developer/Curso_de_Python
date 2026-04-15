1cc9d3778ac12ee0f171873e38b69489f04e08ee
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-15 19:17:40 -0300

    basic python exercises

---
+++ estrutura-sequencial/exercicio12.py
@@ -0,0 +1,9 @@
+##Tendo como dados de entrada um arquivo em Gigabytes, construa um algoritmo que faça a conversão para Megabytes, usando a seguinte fórmula:
+
+
+
+Gigabytes = int(input("Gigabytes : "))
+
+Megabytes = Gigabytes * 1024
+
+print(f"O arquivo em Gigas é de {Gigabytes} e em Megasbytes é de {Megabytes}")
