875255abac52037d18547dad4a0e33007c7e30cd
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-15 18:41:31 -0300

    Basic Python Exercises

---
+++ estrutura-sequencial/exercicio09.py
@@ -0,0 +1,8 @@
+##Faça um programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+farhrenheit = float(input("Temperatura em Fahrenheit : "))
+
+
+C = 5 * ((farhrenheit-32) / 9)
+
+print(f"A temperatura em graus Celsius {C}")
