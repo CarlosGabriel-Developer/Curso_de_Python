95ba21be54e19608f7149f128aad0e0b24b344e8
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-17 20:53:33 -0300

    add exercises python

---
+++ estrutura-de-decisao/exercicio02.py
@@ -0,0 +1,7 @@
+##Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+numero = int(input("Digite um numero positivo ou negativo : "))
+
+
+if numero > 0 : 
+    print()
