4131676cb734e5f54ce9ec5766d505e33ce54f00
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-04-24 19:53:29 -0300

    add

--- estrutura-de-decisao/exercicio08.py
+++ estrutura-de-decisao/exercicio08.py
@@ -2,6 +2,14 @@
 
 menor_valor = 0 
 
-for i in range (1,4,1):
+for i in range(1, 4):
     
-    produto = float(input("Digite o valor do produto : "))
+    produto = float(input("Digite o valor do produto: "))
+    
+    if i == 1: 
+        menor_valor = produto
+    else: 
+        if produto < menor_valor:
+            menor_valor = produto
+
+print(f"O menor produto é o do valor de {menor_valor}")
