b23a12667d6674d78f17ca83b1a970fe03d90a43
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-07 18:59:50 -0300

    Aula do Curso de Python criado pelo professor Gustavo Guanabara

---
+++ Modulo 03/exer75-analise-tupla.py
@@ -0,0 +1,23 @@
+##Exercício Python 075: Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
+
+#A) Quantas vezes apareceu o valor 9.
+#B) Em que posição foi digitado o primeiro valor 3.
+#C) Quais foram os números pares.
+
+num1 = int(input("digite um numero entre 0-9 : "))
+num2 = int(input("digite um numero entre 0-9 : "))
+num3 = int(input("digite um numero entre 0-9 : "))
+num4 = int(input("digite um numero entre 0-9 : "))
+
+numeros_tupla = (num1,num2,num3,num4)
+
+print(f"Quantas vezes apareceu o valor 9 : {numeros_tupla.count(9)} veze(s)")
+print(f"Em que posição foi digitado o primeiro valor : {numeros_tupla}")
+print(f"Quais foram os números pares: ") 
+
+
+for i in numeros_tupla : 
+    
+    if i % 2 == 0 :
+        
+        print(f"{i}", end=" ")
