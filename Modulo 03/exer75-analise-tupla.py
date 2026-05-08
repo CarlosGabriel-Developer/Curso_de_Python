a743c16289f846316a0ce021d6671a5c4eddc190
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-08 20:21:44 -0300

    Aula do Curso de Python criado pelo professor Gustavo Guanabara

--- Modulo 03/exer75-analise-tupla.py
+++ Modulo 03/exer75-analise-tupla.py
@@ -12,12 +12,15 @@ num4 = int(input("digite um numero entre 0-9 : "))
 numeros_tupla = (num1,num2,num3,num4)
 
 print(f"Quantas vezes apareceu o valor 9 : {numeros_tupla.count(9)} veze(s)")
-print(f"Em que posição foi digitado o primeiro valor : {numeros_tupla}")
-print(f"Quais foram os números pares: ") 
 
+if 3 in numeros_tupla : 
+    print(f"Em que posição do index foi digitado: {numeros_tupla.index(3)}")
+else : 
+    print("O numero 3 não foi informado á tupla")
 
+print(f"Quais foram os números pares: ",end=" ") 
 for i in numeros_tupla : 
     
     if i % 2 == 0 :
         
-        print(f"{i}", end=" ")
+        print(i,end=" ")
