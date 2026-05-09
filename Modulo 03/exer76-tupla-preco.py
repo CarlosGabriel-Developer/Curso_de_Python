51bbf75e197cb981bb5ae9290bcfb04ac448acf6
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-09 18:02:31 -0300

    Aula do Curso de Python criado pelo professor Gustavo Guanabara

--- Modulo 03/exer76-tupla-preco.py
+++ Modulo 03/exer76-tupla-preco.py
@@ -5,3 +5,19 @@ tupla_materias = (
     "Lápis", 1.75, "Borracha", 2.00, "Caderno", 15.90, "Estojo", 25.00, "Transferidor", 4.20,
     "Compasso", 9.99, "Mochila", 120.32, "Canetas", 22.30, "Livro", 34.90
 )
+
+print("="*40)
+
+print(f'{"Materias escolares":^40}')
+
+print("="*40)
+ 
+for i in range (0,len(tupla_materias)) : 
+    
+    if i % 2 == 0 : 
+        print (f"{tupla_materias[i]:.<32}",end="R$")
+    
+    else :
+        print (f"{tupla_materias[i]:>2.2f}")
+
+print("="*40)
