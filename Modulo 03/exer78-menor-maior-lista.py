1d90f6a6c66bf896c2ce919c6b3b87711d51ecf0
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-12 19:58:21 -0300

    add Aula do Curso de Python criado pelo professor Gustavo Guanabara

---
+++ Modulo 03/exer78-menor-maior-lista.py
@@ -0,0 +1,36 @@
+##Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista
+
+maior = menor = 0
+num = []
+
+for i in range(1,6): 
+    numero = int(input("Digite um numero : "))
+    num.append(numero)
+    print('Numero adicionado')
+
+print('-'*20)    
+print(num)
+print('-'*20)
+
+for i,v in enumerate(num) : 
+
+    if i == 0 : 
+        
+        maior = menor = v 
+        
+    else : 
+        
+        if v < menor : 
+            
+            menor = v
+            
+            
+        elif v > maior : 
+            
+            maior = v 
+            
+       
+
+print(f"O maior numero da lista foi :{maior}") 
+ 
+print(f"O menor numero da lista foi :{menor}")      
