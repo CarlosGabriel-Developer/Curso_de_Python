c663661ba4b77341d708bc4ce1e6c5254950c87a
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-15 20:44:56 -0300

    Lista ordenada sem repetições

--- modulo-03/exer81-extraindo-dados-lista.py
+++ modulo-03/exer81-extraindo-dados-lista.py
@@ -1,29 +1,32 @@
 ##Exercício Python 081: Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
-#A) Quantos números foram digitados.
+#A) Quantos números foram digitados.""
 #B) A lista de valores, ordenada de forma decrescente.
 #C) Se o valor 5 foi digitado e está ou não na lista.
 
-lista_num = []
+numeros = []
 
 while True : 
     
     numero = int(input('Digite um numero : '))
-    lista_num.append(numero)
+    numeros.append(numero)
     
-    print('Numero adicionado')
+    print('Número adicionado')
     
-    user = str(input("Deseja continuar [S/N] ? ")).upper().strip()
-    if user in 'Nn' : 
+    user = str(input('Deseja continuar [S/N] : ')).strip().upper()
+    
+    if user == "N" :
+        
         break
     
-sorted(lista_num)
-    
-print(f'Foram digitados {len(lista_num)} numero(s)')
-print(f"A lista de numeros em ordem decrescente é {list(reversed(lista_num))}")
-if lista_num in 5 : 
-    
+numeros.sort(reverse=True)
+
+
+print(f'Foram digitados {len(numeros)} vezes')
+print(f"A lista de valores, ordenada de forma decrescente {numeros}")
+
+if 5 in numeros : 
     print('O numero 5 foi digitado na lista')
+
+else:
     
-else : 
-    
-    print('O numero 5 não foi digitado ha lista')
+        print('O numero 5 não foi digitado na lista')
