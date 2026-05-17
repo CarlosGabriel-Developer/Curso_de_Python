4ec089ad52a46c1e864a809c626c16bc86a02ca1
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-17 16:56:57 -0300

    Lista composta e análise de dados

--- modulo-03/exer84-lista-composta.py
+++ modulo-03/exer84-lista-composta.py
@@ -7,8 +7,40 @@
 
 banco_dados = []
 pessoas = []
+maior = menor = 0 
 
-
+while True : 
+    
+    pessoas.append(str(input("Digite o seu nome  :")))
+    
+    pessoas.append(float(input("Digite o seu peso : ")))
+    
+    if len(banco_dados) == 0 : 
+        
+        maior = menor = pessoas[1]
+        
+    else : 
+        
+        if pessoas[1] < menor : 
+            menor = pessoas[1]
+            
+        if pessoas[1] > maior :
+            maior = pessoas[1]
+            
+             
+    banco_dados.append(pessoas[:])
+    pessoas.clear()
+    
+    usuario = str(input("Deseja continuar : [S/N]")).upper().strip()
+    if usuario in "N" : 
+        break
+    
 print("="*30)
 print(banco_dados)
 print("="*30)
+
+print(f"Quantas pessoas foram cadastradas : {len(banco_dados)}")
+
+print(f"Uma listagem com as pessoas mais pesadas {maior}")
+
+print(f"Uma listagem com as pessoas mais leves {menor}")
