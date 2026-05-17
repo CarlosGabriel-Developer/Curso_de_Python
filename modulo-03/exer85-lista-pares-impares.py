f59090732bae25683c2cd35c64d7246e3165ed15
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-17 17:43:22 -0300

    Listas com pares e ímpares

--- modulo-03/exer85-lista-pares-impares.py
+++ modulo-03/exer85-lista-pares-impares.py
@@ -1,33 +1,27 @@
 ##Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente
 
-principal = []
-
-pares = []
-impares = []
-
+principal = [[],[]]
 
 for i in range(0,7,1) : 
     
-    principal.append(int(input(f"Digite um numero no index {i}: ")))
+    numero = (int(input(f"Digite um numero no index {i}: ")))
     
-for i,v in enumerate(principal) : 
-    
-    if v % 2 == 0 : 
+
+    if numero % 2 == 0 : 
         
-        pares.append(v)
+        principal[0].append(numero)
         
-    elif v % 2 == 1 : 
+    elif numero % 2 == 1 : 
         
-        impares.append(v)
+        principal[1].append(numero)
 
 
 print("="*40)   
-print(sorted(principal))
-print("="*40)
 
-print(f"Os valores pares digitados foram : {sorted(pares)}")
+print(f"Os valores pares digitados foram : {sorted(principal[0])}")
 
-print(f"Os valores impares digitados foram : {sorted(impares)}")
+print(f"Os valores impares digitados foram : {sorted(principal[1])}")
+
 print("="*40)
 
     
