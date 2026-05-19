1ab9bd5d3c0f8fbf99891dbc5fad685ff67a0242
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-19 20:15:25 -0300

    Fix  Matriz em Python

--- modulo-03/exer86-matriz-python.py
+++ modulo-03/exer86-matriz-python.py
@@ -1,25 +1,19 @@
 ##Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 
 
-matriz = []
-teste = []
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
 
-
-for c in range (0,3,1) : 
+for l in range(0,3) : 
     
-    for l in range(0,3,1) : 
+    for c in range(0,3) : 
         
-        teste.append(int(input(f"Digite um numero na posição [{c}] [{l}] : ")))
-        matriz.append(teste[:])
-        teste.clear()
-
-
+        matriz[l][c] = int(input(f"Digite um valor para [{l}] [{c}] : "))
+        
 print("="*40)
-for l in range(0,3,1) : 
-    
-    for c in range(0,3,1) : 
-        print(f"{matriz[l][c]}", end=" ")
-    
-    
+ 
+for l in range(0,3) : 
+    for c in range(0,3): 
+        print(f"[{matriz[l][c]:^5}]", end=" ")
+    print()
     
 print("="*40)
