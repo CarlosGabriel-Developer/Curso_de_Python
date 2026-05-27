e3ba3c75f3d890c4a085ba8bc0a6f0f875faf05a
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-05-27 20:39:18 -0300

    add

---
+++ modulo-03/exer90-dicionario-python.py
@@ -0,0 +1,7 @@
+##Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário. No final, mostre o conteúdo da estrutura na tela.
+
+dicionario = {}
+
+nome = str(input("Digite o seu nome :"))
+
+nota = float(input("Digite a sua média : "))
