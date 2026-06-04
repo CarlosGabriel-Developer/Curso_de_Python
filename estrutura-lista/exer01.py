4abd7fe7bc2cd9850d900808db3e8ad0686b4394
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-04 16:40:50 -0300

    add

---
+++ estrutura-lista/exer01.py
@@ -0,0 +1,9 @@
+#Faça um programa que leia um vetor de 5 números inteiros e mostre-os.
+
+lista = []
+
+for i in range(5) : 
+    
+    lista.append(int(input("Digite um numero : ")))
+    
+print(lista)
