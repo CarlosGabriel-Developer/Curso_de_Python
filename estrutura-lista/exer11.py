861c2d93d571b5ee8d6049fa894d6be39aa7c690
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-10 21:31:30 -0300

    add

--- estrutura-lista/exer11.py
+++ estrutura-lista/exer11.py
@@ -1,2 +1,24 @@
 ##Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de 13 anos possuem altura inferior à média de altura desses alunos.
 
+import random
+
+lista_alunos = []
+
+soma_altura = 0
+media_altura = 0 
+
+
+for i in range(30) : 
+    
+    idade_aluno = (random.randint(10,18))
+    altura_aluno = (random.uniform(1.40,1.90))
+    
+    soma_altura += altura_aluno
+    
+    
+    if idade_aluno > 13 and altura_aluno > media_altura: 
+        
+        lista_alunos.append(i)
+        
+print(f"Ao total foram {len(lista_alunos)} alunos maiores de 13 anos")
+    
