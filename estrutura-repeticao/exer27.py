0dd705638a13f08bb2f27595149514af4f924138
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-13 10:12:34 -0300

    add

---
+++ estrutura-repeticao/exer27.py
@@ -0,0 +1,11 @@
+#O Sr. Manoel Joaquim acaba de adquirir uma panificadora e pretende implantar a metodologia da tabelinha, que já é um sucesso na sua loja de 1,99. Você foi contratado para desenvolver o programa que monta a tabela de preços de pães, de 1 até 50 pães, a partir do preço do pão informado pelo usuário
+
+valor_pao = float(input("Preços dos pães :"))
+
+print("=== Tabela de Preços ===")
+
+for i in range(50) : 
+    
+    print(f"Nº{i+1:>3} -- R${(i+1)*valor_pao:>4.2f}")
+    
+print("="*24)
