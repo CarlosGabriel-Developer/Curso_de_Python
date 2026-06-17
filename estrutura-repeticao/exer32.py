30bd0f988c4c6c7480c9d433e8e946f49ff15a65
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-17 20:53:03 -0300

    add

--- estrutura-repeticao/exer32.py
+++ estrutura-repeticao/exer32.py
@@ -1,17 +1,17 @@
 # Encontrar números primos é uma tarefa difícil. Faça um programa que gera uma lista dos números primos existentes entre 1 e um número inteiro informado pelo usuário.
 # Código
 
-num = int(input("Digite um numero: "))
+n = int(input("Digite um número inteiro: "))
 
-print("Números primos:")
+print(f"Números primos entre 1 e {n}:")
 
-for n in range(2, num + 1):
+for num in range(2, n + 1):
     primo = True
 
-    for i in range(2, n):
-        if n % i == 0:
+    for divisor in range(2, int(num ** 0.5) + 1):
+        if num % divisor == 0:
             primo = False
             break
 
     if primo:
-        print(n)
+        print(num, end=" ")
---
+++ estrutura-repeticao/exer36.py
@@ -0,0 +1,12 @@
+#Desenvolva um programa que faça a tabuada de um número qualquer inteiro que será digitado pelo usuário, mas a tabuada não deve necessariamente iniciar em 1 e terminar em 10, o valor inicial e final devem ser informados também pelo usuário, conforme exemplo abaixo:
+
+
+num = int(input("Montar a tabuada de: "))
+comeco = int(input("Começar por: "))
+termina = int(input("Terminar em:"))
+  
+print(f"Vou montar a tabuada de {num} começando em {comeco} e terminando em {termina}")
+
+for i in range (comeco,termina) : 
+    
+    print()
