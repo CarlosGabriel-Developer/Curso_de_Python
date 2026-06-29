b9f32ee305aa4d5b20a3ed3628d1d3022aa7dc8c
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-29 17:25:49 -0300

    Voltando das ferias

--- modulo-03/exer91-jogo-dados.py
+++ modulo-03/exer91-jogo-dados.py
@@ -1,6 +1,17 @@
 ##Exercício Python 091: Crie um programa onde 4 jogadores joguem um dado e tenham resultados aleatórios. Guarde esses resultados em um dicionário em Python. No final, coloque esse dicionário em ordem, sabendo que o vencedor tirou o maior número no dado.
 
-import random
+from random import randint
 
-lista_resultados = []
+num_jogados = []
 
+for i in range(4):
+    num_jogados.append(randint(1, 6))
+
+print("Esses foram os números jogados:")
+print(num_jogados)
+
+maior = max(num_jogados)
+vencedor = num_jogados.index(maior) + 1
+
+print(f"O maior número foi {maior}.")
+print(f"O vencedor foi o Jogador {vencedor}.")
