eee3bc7f356abac983c6daf3f47fcd35bd5aebe6
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-06-30 19:57:19 -0300

    add

---
+++ estrutura-repeticao/exer37.py
@@ -0,0 +1,28 @@
+#Uma academia deseja fazer um censo entre seus clientes para descobrir o mais alto, o mais baixo, a mais gordo e o mais magro, para isto você deve fazer um programa que pergunte a cada um dos clientes da academia seu código, sua altura e seu peso. O final da digitação de dados deve ser dada quando o usuário digitar 0 (zero) no campo código. Ao encerrar o programa também deve ser informados os códigos e valores do clente mais alto, do mais baixo, do mais gordo e do mais magro, além da média das alturas e dos pesos dos clientes
+soma_alturas = 0
+soma_pesos = 0
+quatidade = 0 
+
+
+while True : 
+    
+    cod_cliente = int(input("Diga o seu codigo : "))
+    
+    if cod_cliente == 0 : 
+        break
+    
+    altura = float(input("Altura : "))
+    peso = float(input("Peso : "))
+    
+    if quatidade == 0 : 
+        
+        maior_altura = menor_altura = altura
+        cod_maior = cod_menor = cod_cliente
+        
+        maior_peso = menor_peso = peso
+        cod_maior = cod_menor = cod_cliente
+        
+    if 
+        
+        
+    
