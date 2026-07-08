b77a3bc1315f08a17cdb63c881dbfb18e8a2b906
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-07-08 20:31:01 -0300

    add/fix

--- modulo-03/exer93-ficha-jogador.py
+++ modulo-03/exer93-ficha-jogador.py
@@ -1,16 +1,21 @@
 #Exercício Python 093: Crie um programa que gerencie o aproveitamento de um jogador de futebol. O programa vai ler o nome do jogador e quantas partidas ele jogou. Depois vai ler a quantidade de gols feitos em cada partida. No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
 
-
-lista = []
+soma_gols = 0 
 
 dicionario = {}
 
-dicionario['Nome'] = str(input('Nome : '))
-dicionario['Numero_partidas'] = int(input('Numero de Partidas : '))
+dicionario['Nome'] = str(input("Nome : "))
+dicionario['Numero_partidas'] = int(input("Numero de Partidas jogadas : "))
 
-if dicionario['Numero_partidas'] > 0 :
+if dicionario['Numero_partidas'] > 0 : 
     
     for i in range(dicionario['Numero_partidas']) : 
+        num_gols = int(input(f"Quantos gols foram marcados na partida {i+1} : "))
+        soma_gols += num_gols
         
-        dicionario['gols_partidas'] = int(input(f'Quantos gols na partida {i} : '))
+        dicionario['Numero_total_gols'] = soma_gols
         
+print('-='*40)
+
+for k,v in dicionario.items() : 
+    print(f'A chave {k} tem o valor {v}')
