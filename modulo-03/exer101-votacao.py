9477485db526aa91a97591b42e0b023240fac2ca
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-01 15:55:39 -0300

    add

--- modulo-03/exer101-votacao.py
+++ modulo-03/exer101-votacao.py
@@ -1,11 +1,9 @@
-from datetime import datetime
-
-
-ano_atual = datetime.now().year
-
+#Exercício Python 101: Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.
 
 def voto(ano_nascimento) : 
-    
+    from datetime import datetime
+    ano_atual = datetime.now().year
+
     idade_atual = ano_atual- ano_nascimento
 
 
---
+++ modulo-03/exer102-fatorial.py
@@ -0,0 +1,10 @@
+
+
+def fatorial (numero=1,show=0) :
+    
+    f = 1
+    
+    for i in range(numero,0,-1):
+        f*= i
+    
+    return i 
