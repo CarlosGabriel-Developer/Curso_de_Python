3c816d9e5c4112b1b1ef166973be33e81d89e407
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-01 16:04:49 -0300

    add

--- modulo-03/exer102-fatorial.py
+++ modulo-03/exer102-fatorial.py
@@ -2,9 +2,22 @@
 
 def fatorial (numero=1,show=0) :
     
+    """Fatorial(Numero, show= False)
+    
+    --> Calcula o fatorial de um numero
+    Para Numero:O numero a ser calculado
+    Para Show:(opcional) Mostrar ou não contar
+    Return: O valor fatorial do numero
+    
+    """
+    
     f = 1
     
     for i in range(numero,0,-1):
         f*= i
     
-    return i 
+    return f
+
+numero = int(input('Numero: '))
+
+print(fatorial(numero))
