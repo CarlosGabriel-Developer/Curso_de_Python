50dffa5928024bb2d40ee6ff394d434c06bc6b1a
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-03 21:38:01 -0300

    add

--- exer104-entrada-dados.py
+++ exer104-entrada-dados.py
@@ -1,15 +1,29 @@
 #Exercício Python 104: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante 'a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
 # Ex: n = leiaInt('Digite um n: ')
 
-def leiaInt(num='') : 
+def leiaInt(numero='') : 
     
-    if num.isnumeric : 
-        return f"Voce acabou de digita {num}"
+    ok = False
     
-    else : 
+    valor = 0 
+    while True : 
         
-        return ('\033[31m Erro,Digite um numero válido]')
+        num = str(input(numero))
+        
+        if num.isnumeric() : 
+            valor = int(num)
+            ok = True
+            
+        else : 
+            print('\033[0;31mErro,Tente outro numero\033[m')
+            
+        if ok :
+            break
+        
+    return valor
 
+num = leiaInt("Digite um numero: ")
 
-
+print(f"O numero digitado foi {num}")
+            
 
