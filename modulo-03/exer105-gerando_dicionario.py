e35d37accb5697ef64702925f5bf3fc4fadd3065
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-05 20:12:05 -0300

    add

--- modulo-03/exer105-gerando_dicionario.py
+++ modulo-03/exer105-gerando_dicionario.py
@@ -7,9 +7,30 @@
     # A situação (opcional)
 
 
-def notas(*nota,sit=True) : 
+def notas(*nota,sit=False) : 
+    
+    ''''''
     
     dados = {}
     
-
+    dados['Total'] = len(nota)
+    dados['Maior'] = max(nota)
+    dados['Menor'] = min(nota)
+    dados['Media'] = sum(nota)/len(nota)
     
+    if sit :
+        if dados['Media'] >= 7 :
+            dados['Situação'] = 'BOA'
+            
+        elif dados['Media'] >= 5 :
+            dados['Situação'] = 'RAZOAVEL'
+            
+        else :
+            dados['Situação'] = 'RUIM'
+            
+    return dados
+    
+
+usuario = notas(23,2,12,3,sit=True)
+
+print(usuario)
