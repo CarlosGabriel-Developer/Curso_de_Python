54a74e043a00865ceb7b5e9c910e7482e9bd7bab
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-09 17:18:17 -0300

    teste

--- modulo-03/teste.py
+++ modulo-03/teste.py
@@ -8,15 +8,23 @@ def voto(idade) :
     
     idade = ano_atual - ano_nacismento
     
-    print(f'A sua idade é {idade}')
+    print(f'A sua idade é {idade} anos')
     
     if idade >= 18 : 
         
-        return f'O voto é obrigatorio com {idade}'
+        return f'O voto é obrigatorio com {idade} anos'
+    
+    elif 16 <= idade or idade >= 65 : 
+        
+        return f'O voto com  a idade de {idade} anos é opcional'
+    
+    else : 
+        
+        return 'O voto ainda não possivel'
 
 
 
 
-ano_nacismento = int(input('Ano de Nacimento'))
+ano_nacismento = int(input('Ano de Nacimento: '))
 
 print(voto(ano_nacismento))
