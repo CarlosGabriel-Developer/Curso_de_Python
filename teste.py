2231bc9256ff0e0101d501c88e7e85e2c269635c
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-11 19:23:56 -0300

    add

--- teste.py
+++ teste.py
@@ -22,37 +22,37 @@
          
 # numero_maior()
 
-import random
-lista_numeros = []
+# import random
+# lista_numeros = []
 
-def sortreio () : 
+# def sortreio () : 
     
-    lista_numeros.clear()
+#     lista_numeros.clear()
 
-    for i in range(5):
+#     for i in range(5):
         
-        lista_numeros.append(random.randint(0,100))
+#         lista_numeros.append(random.randint(0,100))
         
-    print('='*30)
-    print('A lista dos numeros é de :')
-    print(lista_numeros)
-    print('='*30)
+#     print('='*30)
+#     print('A lista dos numeros é de :')
+#     print(lista_numeros)
+#     print('='*30)
         
-def soma_par() : 
+# def soma_par() : 
     
-    soma_numeros_pares = 0 
-    contador_numero_pares = 0 
+#     soma_numeros_pares = 0 
+#     contador_numero_pares = 0 
     
-    for i in lista_numeros : 
-        if i % 2 == 0 : 
-            soma_numeros_pares += i 
-            contador_numero_pares += 1 
+#     for i in lista_numeros : 
+#         if i % 2 == 0 : 
+#             soma_numeros_pares += i 
+#             contador_numero_pares += 1 
             
-    print(f'Foi informado {contador_numero_pares} numeros pares')
-    print('='*30)
-    print(f'A soma dos numeros pares é de {soma_numeros_pares}')
-    print('='*30)
+#     print(f'Foi informado {contador_numero_pares} numeros pares')
+#     print('='*30)
+#     print(f'A soma dos numeros pares é de {soma_numeros_pares}')
+#     print('='*30)
             
 
-sortreio()
-soma_par()
+# sortreio()
+# soma_par()
