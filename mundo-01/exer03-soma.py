b7f6a5deb673a4ddacfc89a8b2f8f34ac4769626
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-27 19:21:16 -0300

    add

--- mundo-01/exer03-soma.py
+++ mundo-01/exer03-soma.py
@@ -1,9 +1,14 @@
-num01 = float(input("digite um numero : "))
-num02 = float(input("digite outro numero : "))
+while True:
 
-soma = num01 + num02
+    try:
 
-
-print(f"\033[1;34m{num01} + {num02} == \033[31m{soma}\033[0m")
+        num01 = float(input("digite um numero : "))
+        num02 = float(input("digite outro numero : "))
+        soma = num01 + num02
+        
+        print(f"\033[1;34m{num01} + {num02} == \033[31m{soma}\033[0m")
+        
+    except :
+        print('Apenas valores numericos')
 
 
