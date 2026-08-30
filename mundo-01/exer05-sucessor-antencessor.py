3365e2b5002872c58a2d3206e0c2d4cf5b11677e
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-30 17:14:10 -0300

    exerption usado

--- mundo-01/exer05-sucessor-antencessor.py
+++ mundo-01/exer05-sucessor-antencessor.py
@@ -4,9 +4,10 @@ try:
     
     numero = int(input("Digte um numero : "))
     
-except :
-    
-    print('Apenas numéricos')
+except Exception as erro:
+    print()
+    print(f'O tipo de erro foi {erro}')
+    print('Apenas numeros são validos')
 
 else: 
     print(f"O seu numero é {numero} o seu antercessor é {numero-1} e o seu sucessor vai ser o {numero+1}")
