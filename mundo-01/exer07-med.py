8c542bdf1d90c0c121e6e4949d29ac371cd24847
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-30 17:19:08 -0300

    add tratamento de erros

--- mundo-01/exer07-med.py
+++ mundo-01/exer07-med.py
@@ -1,6 +1,11 @@
-nota1 = float(input("Digite a sua primeira nota :"))
-nota2 = float(input("Digite a sua segunda nota :"))
+try: 
 
-media = (nota2 + nota1) / 2
-
-print(f'A media da nota do aluno é {media}')
+    nota1 = float(input("Digite a sua primeira nota :"))
+    nota2 = float(input("Digite a sua segunda nota :"))
+    
+except : 
+    print('Apenas numeros validos')
+    
+else:
+    media = (nota2 + nota1) / 2
+    print(f'A media da nota do aluno é {media}')
