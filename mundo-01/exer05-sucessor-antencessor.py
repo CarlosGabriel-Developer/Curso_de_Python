ce20d3c57274bbf7d8c91c7d7d37db34cac0a752
Carlos Gabriel Sousa <carlos.desenvolvedor.dev@gmail.com>
2026-08-30 16:59:35 -0300

    tratamento de erros

--- mundo-01/exer05-sucessor-antencessor.py
+++ mundo-01/exer05-sucessor-antencessor.py
@@ -1,9 +1,12 @@
 
-numero = int(input("Digte um numero : "))
 
+try: 
+    
+    numero = int(input("Digte um numero : "))
+    
+except :
+    
+    print('Apenas numéricos')
 
-az_num = (f"\033[1;34m{numero+1}\033[0m")
-
-vm_num = (f"\033[1;31m{numero-1}\033[0m")
-
-print(f"O seu numero é {numero} o seu antercessor é {vm_num} e o seu sucessor vai ser o {az_num}")
+else: 
+    print(f"O seu numero é {numero} o seu antercessor é {numero-1} e o seu sucessor vai ser o {numero+1}")
